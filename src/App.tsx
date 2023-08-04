dfab42c3515a0a8e2b94033e2e8c3f87a0d94c9c
Codux <codux@codux.com>
2023-08-04 12:19:47 +0800

    update header

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import styles from './App.module.scss';
 import { Label } from './components/label/label';
 import { Input } from './components/input/input';
 import { SubmitButton } from './components/submit-button/submit-button';
+import Classnames from 'classnames';
+import Input_module from './components/input/input.module.scss';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.App}>
-            <h1>My Header</h1>
+            <h1>Contact Us</h1>
+            <h2></h2>Have a question? Fill out the form below and we&apos;ll get{' '}
+            <h3 className={Input_module.root}>back to you as soon as we can.</h3>
             <form>
                 <Label htmlFor="name">Name</Label>
                 <Input id="name" name="name" />
